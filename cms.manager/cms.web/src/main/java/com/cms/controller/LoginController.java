cd870c0a81a8178cdd70a401a537ca27191d9044
ch3nwe1 <33016153+ch3nwe1@users.noreply.github.com>
2018-06-08 21:02:57 +0800

    Initial commit

---
+++ cms.manager/cms.web/src/main/java/com/cms/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.cms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("/")
+	private String login(){
+		return "index";
+	}
+}
